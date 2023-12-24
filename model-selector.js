7b81f4cb17844c855aed3a30a312ff364b3290ba
fjt001 <frank.tudor@gmail.com>
2023-12-24 15:56:57 -0600

    improved model descripts and possibly handling. GPT3.5 is not playing nice

--- model-selector.js
+++ model-selector.js
@@ -3,24 +3,24 @@ const vscode = require('vscode');
 const MODELS = {
     ada: {
         name: 'text-ada-001',
-        description: 'Ada (Cost-effective for basic tasks)'
+        description: 'Ada (Fast and cost-effective for basic tasks like grammar and spelling corrections. Less nuanced understanding.)'
     },
     babbage: {
         name: 'text-babbage-001',
-        description: 'Babbage (Moderately priced, good for general tasks)'
+        description: 'Babbage (Balanced for straightforward tasks and semantic search ranking. Good for simple classification.)'
     },
     curie: {
         name: 'text-curie-001',
-        description: 'Curie (Higher quality, more expensive)'
+        description: 'Curie (Powerful and fast for nuanced tasks such as sentiment classification, summarization, and question answering.)'
     },
     davinci: {
         name: 'text-davinci-003',
-        description: 'Davinci (Highest quality, most expensive)'
+        description: 'Davinci (Highest quality and most capable. Ideal for complex tasks requiring deep understanding and creative content generation.)'
     },
     gpt35: {
         name: 'gpt-3.5-turbo',
-        description: 'GPT-3.5 Turbo (Cost-effective for advanced tasks)'
-    }
+        description: 'GPT-3.5 Turbo (Cost-effective for advanced tasks. Optimized for chat, suitable for interactive applications requiring conversational AI.)'
+    } // Ensure that this closing brace is correctly placed
 };
 
 async function selectModel(context) {
